7b9770f65b253392f540ab92f3b9dabe227b5fd4
Revathi Chandana <chandanarevathi30@gmail.com>
2020-06-10 20:20:37 +0530

    uc-6 : store result in a dictionary

--- arithematic_computation_sorting.sh
+++ arithematic_computation_sorting.sh
@@ -4,7 +4,10 @@ read -p " enter 1st number : " a
 read -p " enter 2nd number : " b
 read -p " enter 3rd number : " c
 echo " a=$a   b=$b   c=$c "
-echo " a + b * c = " $(($a+$b*$c))
-echo " a * b + c = " $(($a*$b+$c))
-echo " c + a / b = " $(($c+$a/$b))
-echo " a % b + c = " $(($a%$b+$c))
+declare -A storeResult
+storeResult[0]=$(($a+$b*$c))
+storeResult[1]=$(($a*$b+$c))
+storeResult[2]=$(($c+$a/$b))
+storeResult[3]=$(($a%$b+$c))
+echo "keys : " ${!storeResult[@]}
+echo "values : " ${storeResult[@]}
