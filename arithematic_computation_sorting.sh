0e54f2f330d1eca24bb445e612f0cc73df92ed94
Revathi Chandana <chandanarevathi30@gmail.com>
2020-06-10 20:03:32 +0530

    uc-4 : compute c + a / b

--- arithematic_computation_sorting.sh
+++ arithematic_computation_sorting.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/bash -x
 echo "This problem computes different arithematic expressions and sort the results"
 read -p " enter 1st number : " a
 read -p " enter 2nd number : " b
@@ -6,3 +6,4 @@ read -p " enter 3rd number : " c
 echo " a=$a   b=$b   c=$c "
 echo " a + b * c = " $(($a+$b*$c))
 echo " a * b + c = " $(($a*$b+$c))
+echo " c + a / b = " $(($c+$a/$b))
