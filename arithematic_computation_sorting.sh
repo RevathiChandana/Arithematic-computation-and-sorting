9330f3c2a8dc2fa5bdfd6c31d3fc84fa4db25788
Revathi Chandana <chandanarevathi30@gmail.com>
2020-06-21 10:59:38 +0530

    uc-1 :Display given numbers

--- arithematic_computation_sorting.sh
+++ arithematic_computation_sorting.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 echo "This problem computes different arithematic expressions and sort the results"
-read -p " enter 1st number : " a
-read -p " enter 2nd number : " b
-read -p " enter 3rd number : " c
-echo "a=$a   b=$b   c=$c "
+read -p "Enter 3 number : " a b c
+DisplayNumbers()
+{
+	echo "a=$a   b=$b   c=$c "
+}
+DisplayNumbers $a $b $c
